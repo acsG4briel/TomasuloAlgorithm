bf4e088cd530b3a51a6ad5f1b2be41192ec6fb80
Gabriel Silva <gabriel.araujocsilv@gmail.com>
2025-06-01 21:31:03 -0300

    PARTIAL - Alteração no valor dos registradores

--- main.cpp
+++ main.cpp
@@ -88,7 +88,7 @@ int main()
     vector<StatusRegistrador> StatusRegistradores = instanciarStatusRegistradores();
 
     // Instância registradores
-    vector<int> Registradores = {VALOR_REGISTRADOR_0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    vector<int> Registradores = {VALOR_REGISTRADOR_0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
 
     // Inicio da simulação
     cout << "Valores Iniciais:" << endl;
